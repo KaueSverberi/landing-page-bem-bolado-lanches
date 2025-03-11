ce15f79ffde7a87f3cfca4716723366322aafbf1
KaueSverberi <miletkaue@gmail.com>
2025-03-10 21:55:02 -0300

    adicionei js e arrumei o css

---
+++ scripts.js
@@ -0,0 +1,14 @@
+document.getElementById('select-cardapio').addEventListener('change', function() {
+    const selectedValue = this.value;
+    
+    // Esconde todas as listas
+    document.getElementById('lanches').style.display = 'none';
+    document.getElementById('porcao').style.display = 'none';
+    document.getElementById('acai').style.display = 'none';
+    
+    // Exibe a lista correspondente à opção selecionada
+    document.getElementById(selectedValue).style.display = 'grid';
+});
+
+// Exibe a lista inicial (opção padrão) ao carregar a página
+document.getElementById('select-cardapio').dispatchEvent(new Event('change'));
